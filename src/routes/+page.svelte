644229a06c4c2355614dded13ecb9da1aae6d0bc
Dominik Röschke <dominik.roeschke@web.de>
2024-02-05 12:09:14 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { FileDropzone } from '@skeletonlabs/skeleton';
+    
+    let files = undefined;
+    let fileInput;
+
+    $: console.log(fileInput);
+</script>
+
+<form method="POST" action="/"> 
+    <FileDropzone bind:fileInput={fileInput} multiple accept="image/*" name="signage" bind:files/>
+
+    <button class="btn variant-ghost-warning" on:click={() => (files = undefined)} type="button">Won't reset files</button>
+    <button class="btn variant-filled-error" on:click={() => fileInput.value = ''} type="button">
+        Will reset files
+    </button>
+    <button class="btn variant-ringed-primary" type="submit">Submit</button>
+</form>
