7c964d6cebb9c1bf84939b83deb32ededf059ec0
Dominik Röschke <dominik.roeschke@web.de>
2024-02-05 15:46:23 +0100

    Updated examples

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,5 +3,6 @@ export const actions = {
     default: async ({request}) => {
       const formData = await request.formData();
       console.log('foo',formData.get('signage'));
+      console.log('vanilla',formData.get('vanilla'));
     }
   }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,22 @@
     
     let files = undefined;
     let fileInput;
+    let vanillaInput;
 
-    $: console.log(fileInput);
+    $: console.log("Skeleton:", fileInput);
+    $: console.log("Vanilla: ", vanillaInput);
 </script>
 
-<form method="POST" action="/"> 
+<form method="POST" action="/" enctype="multipart/form-data"> 
     <FileDropzone bind:fileInput={fileInput} multiple accept="image/*" name="signage" bind:files/>
 
     <button class="btn variant-ghost-warning" on:click={() => (files = undefined)} type="button">Won't reset files</button>
-    <button class="btn variant-filled-error" on:click={() => fileInput.value = ''} type="button">
+    <button class="btn variant-filled-error" on:click={() => fileInput.value = null} type="button">
         Will reset files
+    </button>
+    <input type="file" bind:this={vanillaInput} name="vanillainput" multiple/>
+    <button class="btn variant-filled-error" on:click={() => vanillaInput.value = null} type="button">
+        reset vanilla input
     </button>
     <button class="btn variant-ringed-primary" type="submit">Submit</button>
 </form>
